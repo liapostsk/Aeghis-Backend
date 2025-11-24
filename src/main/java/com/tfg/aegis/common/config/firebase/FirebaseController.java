4071ed5f0f6cd82a5c41058919a8fcaf828ce9af
Lia Lopx <lialopx@macbook-pro-de-lia-1.home>
2025-11-24 19:57:59 +0100

    eliminado mapper customer y tokenresponse de firebase

--- src/main/java/com/tfg/aegis/common/config/firebase/FirebaseController.java
+++ src/main/java/com/tfg/aegis/common/config/firebase/FirebaseController.java
@@ -1,10 +1,9 @@
 package com.tfg.aegis.common.config.firebase;
-;
+
 import com.google.firebase.auth.FirebaseAuthException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
